74b6180eafa4ae4bd5218a20bae416da50022101
Clay Simmons <clay.simmons@gmail.com>
2013-11-11 15:33:48 -0500

    Need log file as cli argument.

--- bin/proxy.js
+++ bin/proxy.js
@@ -11,6 +11,11 @@ if ( !options["parser-path"] ) {
   options["parser-path"] = "~/Dropbox/12c/parser-sportsnetwork";
 }
 
+if ( !options.logs ) {
+  console.log("need to know path to log file. Use option --logs");
+  process.exit(1);
+}
+
 proxy = new Proxy( options );
 
 process.stdin
