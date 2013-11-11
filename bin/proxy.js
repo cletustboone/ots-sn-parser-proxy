dc5e4476849dd1656445b0570ae0952d6e6e17a0
Clay Simmons <clay.simmons@gmail.com>
2013-11-11 11:42:26 -0500

    Sport determined from heading or file name. No longer a required argument. This is so we can pass it as an argument to the parser. Helps with unified logging of parser.

--- bin/proxy.js
+++ bin/proxy.js
@@ -7,21 +7,17 @@ split = require("split"),
 matcher = options.in0 ? "\0" : null,
 proxy;
 
-if ( !options.sport ) {
-  console.log("Need to know what sport you'd like to monitor. Use option --sport (nfl|mlb)");
-  process.exit(1);
-} else {
-  if ( !options["parser-path"] ) {
-    options["parser-path"] = "~/Dropbox/12c/parser-sportsnetwork";
-  }
-  proxy = new Proxy( options );
-
-  process.stdin
-    .pipe( split( matcher ) )
-    .pipe( proxy )
-    .pipe( process.stdout );
-
-  proxy.on( "error", function( err ) {
-    console.log( err );
-  });
+if ( !options["parser-path"] ) {
+  options["parser-path"] = "~/Dropbox/12c/parser-sportsnetwork";
 }
+
+proxy = new Proxy( options );
+
+process.stdin
+  .pipe( split( matcher ) )
+  .pipe( proxy )
+  .pipe( process.stdout );
+
+proxy.on( "error", function( err ) {
+  console.log( err );
+});
